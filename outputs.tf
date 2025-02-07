1d8570d3943f8b702c3b5dde859da4cdec27655c
Leandro Borges <leandroborges_br@hotmail.com>
2025-02-07 10:14:58 -0500

    Triiger

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,11 @@
 output "vpc_id" {
   value       = nonsensitive(module.main.vpc_id)
   description = "value of the VPC ID"
-  sensitive = false
+  sensitive   = false
 }
 
 output "public_subnets" {
   value       = nonsensitive(module.main.public_subnets)
   description = "value of the public subnets"
-  sensitive = false
+  sensitive   = false
 }
